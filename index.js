5176cb1e61a97c9fc1f7d3dbf106ed4d490513ad
hambali-020608 <subastianhambali@gmail.com>
2026-01-23 10:38:25 +0700

    add compression

--- index.js
+++ index.js
@@ -3,10 +3,11 @@ const app = express();
 const port = 3000;
 const cors = require('cors')
 const apiRoutes = require("./src/routes/index");
+const compression = require("compression");
 
 app.use(cors({origin:'*'}))
 app.use(express.json());
-// app.use(compression());
+app.use(compression());
 app.use("/", apiRoutes)
 
 app.listen(port, () => {
